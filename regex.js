a5f82636450d68e8cbed8cdbcbce1d56cf933259
Ivan Sarpi <idsarpiu@falabella.cl>
2019-12-17 18:18:05 -0300

    better regex with negatives

--- regex.js
+++ regex.js
@@ -1,4 +1,15 @@
-const x = '//~\n1~2~3\n4~5';
-const y = x.match(/\d+/g);
-const z = y.reduce((val, acc) => parseInt(acc, 10) + parseInt(val, 10));
-console.log(z);
+const add = (param) => {
+  const strs = param.match(/-{0,1}\d+/g);
+  const nums = strs.map((el) => parseInt(el, 10));
+  const neg = nums.some((el) => el < 0);
+  if (neg) throw new Error('negativos no soportados');
+  return nums.reduce((val, acc) => acc + val);
+};
+
+const param = '//~\n1~2~3\n4~5';
+try {
+  const res = add(param);
+  console.log(res);
+} catch (e) {
+  console.log(e.message);
+}
